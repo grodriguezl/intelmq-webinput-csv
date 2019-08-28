77292906ea351ee1599ade288b847da4d87a095e
Guillermo Rodriguez <grodriguezl@users.noreply.github.com>
2019-08-28 14:40:45 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,9 +31,9 @@ setup(
     test_suite='intelmq_webinput_csv.tests',
     packages=find_packages(),
     package_data={'intelmq_webinput_csv': [
-        '/etc/intelmq/webinput.conf',
-        'static',
-        'bin'
+        'etc/webinput.conf',
+        'static/*',
+        'bin/*'
     ]
     },
     include_package_data=True,
